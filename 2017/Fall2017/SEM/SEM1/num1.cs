7b000b82fac7b6bc458c98f542c61f5136f49261
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-11 15:53:50 +0400

    Номер1

---
+++ 2017/Fall2017/SEM/SEM1/num1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = 123;
+            int b = (a/100);
+            b = (a / 100) + (((a % 100) / 10) * 10) + ((a % 10) * 100);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
