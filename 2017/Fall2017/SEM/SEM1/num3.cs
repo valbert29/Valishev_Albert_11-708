d32e8d2dda8f6d97f986b501a93f5d59f1a6b283
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-13 22:47:05 +0300

    Задание5

---
+++ 2017/Fall2017/SEM/SEM1/num3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int v;
+            a = 1899;
+            b = 1913;
+            v = (b - a) / 4 - (b - a) / 100 + (b - a) / 400;
+            Console.WriteLine(v);
+        }
+    }
+}
