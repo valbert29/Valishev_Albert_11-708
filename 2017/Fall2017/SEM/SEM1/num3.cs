ce067cb1647f0eeed10e4c70d18d1cd73337caa4
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-15 21:00:51 +0300

    Задание 3

---
+++ 2017/Fall2017/SEM/SEM1/num3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("H(количество Часов):");
+            int H = Int32.Parse(Console.ReadLine());
+            var P = H;
+            if (P >= 12) P = P - 12;
+            var M = P * 30;
+            if (M > 180) M = 360 - M;
+            string Str = H + " часов -> " + M.ToString() + " градусов";
+            Console.WriteLine(Str);
+        } 
+    }
+}
