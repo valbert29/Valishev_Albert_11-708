a95b4ba498edac1596d26405126dc5698351d840
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-16 19:16:31 +0300

    Задание 6

---
+++ 2017/Fall2017/SEM/SEM1/num6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите X прямой:");
+            int X = Int32.Parse(Console.ReadLine());// ввели числа
+            Console.Write("Введите Y прямой:");
+            int Y = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите X1 прямой:");
+            int X1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите Y1 прямой:");
+            int Y1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите X точки:");
+            int X2 = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите Y точки:");
+            int Y2 = Int32.Parse(Console.ReadLine());
+
+            var L = (Math.Abs((Y - Y1) * X2 - (X - X1) * Y2 + X1 * Y - Y1 * X)) / (Math.Sqrt(Math.Pow((Y1 - Y), 2) + Math.Pow((X1 - X), 2)));
+            Console.WriteLine(L);
+        } 
+    }
+}
