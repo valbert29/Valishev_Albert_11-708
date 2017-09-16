d909bd1bd54628ef852229cb6a1c1cb391ba7a16
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-16 11:39:17 +0300

    Задание 4

---
+++ 2017/Fall2017/SEM/SEM1/num4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N:");
+            int N = Int32.Parse(Console.ReadLine());// ввели числа
+            Console.Write("Введите X:");
+            int X = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите Y :");
+            int Y = Int32.Parse(Console.ReadLine());
+            var K = 0;
+            int i;
+            for (i = 1; i < (N-1); i++)
+            {
+                if (i % X == 0) K++; // считает кол-во чисел с делителем х
+                if (i % Y == 0) K++;//считает с делителем у
+                if (i % (Y * X) == 0) K--; //вычитает повторяющиеся числа
+            }   
+            Console.WriteLine(K);
+        } 
+    }
+}
