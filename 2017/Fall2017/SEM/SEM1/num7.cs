d65816d5c6c5df5396536e418f2b8c3c7067c67b
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-17 11:54:52 +0300

    Задание 7

---
+++ 2017/Fall2017/SEM/SEM1/num7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Уравнение прямой вида: y=ax+b. ");
+            Console.Write("Введите a прямой:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите x прямой:");
+            int x = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите b прямой:");
+            int b = Int32.Parse(Console.ReadLine());
+            var  a1 = (double)-1 / a;
+            string s = "вектор, перпендикулярный прямой, с началом в начале координат имеет координаты   (" + x.ToString()+";"+(a1*x).ToString()+")";
+            Console.WriteLine(s);
+            string p = "вектор, параллельный прямой, с началом в начале координат имеет координаты (" + x.ToString() + ";" + (a * x).ToString() + ")";
+            Console.WriteLine(p);
+        } 
+    }
+}
