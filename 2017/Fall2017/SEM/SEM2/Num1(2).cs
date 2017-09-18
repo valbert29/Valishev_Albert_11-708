d4efff27b9e2660a0911681c9790d091d5e7f3d4
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-18 14:33:37 +0300

    Задание 1

---
+++ 2017/Fall2017/SEM/SEM2/Num1(2).cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            int i;
+            for (i = 1; i < 1000; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    k = k + i;
+                }
+                if (i%3==0)
+                {
+                    k = k + i;
+                }
+                if (i%15==0)
+                {
+                    k = k - i;
+                }
+            }
+            Console.WriteLine(k);
+        }
+
+    }
+}
