09e7f0a5fdf4a56bf66c08d1a72a43dc0b731e0c
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-18 15:09:40 +0300

    Задание2

---
+++ 2017/Fall2017/SEM/SEM2/num2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите значение часовой стрелки");
+                int x = int.Parse(Console.ReadLine());
+            Console.Write("введите значение минутной стрелки");
+                int y = int.Parse(Console.ReadLine());
+            if (x>12)
+            {
+                x -= 12;
+            }
+            double otvet =1.0;
+            double grm = y * 6;
+            double grh = x * 30 + grm / 12;
+            double ugol = Math.Abs(grh - grm);
+            if (ugol >180)
+            {
+                 otvet = (360 - ugol);
+            }
+            else
+            {
+                otvet = ugol;
+            }
+            Console.WriteLine(grm);
+            Console.WriteLine(grh);
+            Console.WriteLine(otvet);
+        }   
+
+
+    }
+}
