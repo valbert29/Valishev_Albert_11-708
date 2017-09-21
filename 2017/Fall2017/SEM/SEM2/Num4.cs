00cfcf097b993090a245fdeb6d4ab88b78dee65c
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-21 21:26:35 +0300

    Задание 4

---
+++ 2017/Fall2017/SEM/SEM2/Num4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)//задача про козла на веревке, который ест траву
+        {
+            string str = Console.ReadLine();
+            double rad, storona;
+            storona = double.Parse(str.Substring(0, str.IndexOf(' ')));
+            str = str.Substring(str.IndexOf(' ') + 1);
+            rad = double.Parse(str);
+            if (storona / 2 >= rad)
+            {
+                Console.WriteLine(Math.Round(rad * rad * Math.PI, 3));
+            }
+            else
+            {
+                if (Math.Pow(rad, 2) - Math.Pow(storona / 2, 2) >= Math.Pow(storona / 2, 2))
+                {
+                    Console.WriteLine(Math.Pow(storona, 2));
+                }
+                else
+                {
+                    Double x, alpha;
+                    x = Math.Sqrt(Math.Pow(rad, 2) - Math.Pow(storona / 2, 2));
+                    alpha = 2 * Math.Asin(x / rad);
+                    double otvet;
+                    otvet = Math.Pow(rad, 2) * Math.PI - 4 * 0.5 * (alpha - Math.Sin(alpha)) * Math.Pow(rad, 2);
+                    Console.WriteLine(Math.Round(otvet, 3));
+                }
+            }
+
+
+        }
+    }
+}
