60736f4767dc65960ad8088a96d688f32ef42573
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-24 17:32:07 +0300

    Семестровое задание 1

---
+++ 2017/Fall2017/PS/Semestrovka1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)//в этой задаче надо определить, можно ли пойти конем на заданную точку
+        {
+            Console.WriteLine("введите f - первый член прогрессии:");
+            int f = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите s - шаг прогрессии:");
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите n - проверяемое значение:");
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            int k = 0;
+            while (a < n)
+            {
+                k += 1;
+                a = f + (k - 1) * s;
+            }
+            if (a==n)
+            {
+                Console.WriteLine(k);
+            }
+            else
+            {
+                Console.WriteLine("-1");
+            }
+                
+            
+        }
+    }
+}
