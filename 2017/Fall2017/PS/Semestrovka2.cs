d25de9779e225a8969a3be18d7bedc52998f5a7d
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-24 17:32:25 +0300

    Семестровое задание 2

---
+++ 2017/Fall2017/PS/Semestrovka2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)//в этой задаче находим порядковый номер числа в прогресиии, 
+        {
+            Console.WriteLine("введите f - первый член прогрессии:");
+            int f = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите s - шаг прогрессии:");
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите n - проверяемое значение:");
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            int k = 0;
+            while (a < n)
+            {
+                k += 1;
+                a = f + (k - 1) * s;
+            }
+            if (a==n)
+            {
+                Console.WriteLine(k);
+            }
+            else
+            {
+                Console.WriteLine("-1");//если n не являтся членом прогрессии, то выводится -1
+            }
+                
+            
+        }
+    }
+}
