ca0d14e5f56a0fb0bcadf8b351995f6b33fc6e57
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-28 23:45:13 +0300

    Задание про отрезки

---
+++ 2017/Fall2017/SEM/SEM3/num2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x фигуры: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("y фигуры: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("z фигуры: ");
+            int z = int.Parse(Console.ReadLine());
+            Console.WriteLine("a отверстия: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("b фигуры: ");
+            int b = int.Parse(Console.ReadLine());
+            int c = (Math.Min(Math.Max(x, y), Math.Max(x, z)));
+            if ((Math.Min(z, Math.Min(x, y)) < Math.Min(a, b)) && (Math.Max(a, b)) > c)
+                Console.WriteLine(true);
+            else Console.WriteLine(false);
+
+        }
+    }
+}
