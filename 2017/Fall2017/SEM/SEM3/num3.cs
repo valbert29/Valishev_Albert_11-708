f353286c3c804e98d85b9ef04b713e9216465e71
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-09-29 00:16:46 +0300

    Задача про счастливый билет

---
+++ 2017/Fall2017/SEM/SEM3/num3.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число: ");
+            int n = int.Parse(Console.ReadLine());
+            if ((((n - 1) % 10) + (((n - 1) / 10) % 10) + (((n - 1) / 100) % 10) ==
+                (((n - 1) / 1000) % 10) + (((n - 1) / 10000) % 10) + (((n - 1) / 100000) % 10))||
+                    (((n + 1) % 10) + (((n + 1) / 10) % 10) + (((n + 1) / 100) % 10) ==
+                (((n + 1) / 1000) % 10) + (((n + 1) / 10000) % 10) + (((n + 1) / 100000) % 10)))
+                Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
