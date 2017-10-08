5345868dbe268d354780362ed48e7e2d06f3944d
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-10-08 18:18:18 +0400

    Loop5

---
+++ 2017/Fall2017/SEM/SEM4/Задание про скобочки))).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите строку символов");
+            int count = 0;
+            int countMain = 0;
+            string str = Console.ReadLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(') count++;
+                if (count > countMain) countMain = count;
+                if (str[i]==')') count--;
+            }
+            if (count != 0)
+                Console.WriteLine("Uncorrect");
+            else Console.WriteLine("Correct, максимальная глубина: "+countMain);
+        }
+    }
+}
