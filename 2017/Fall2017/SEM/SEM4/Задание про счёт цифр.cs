3652b7c6c93ac694def3ac547a61b07425a3a37e
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-10-08 16:56:52 +0400

    Loops1

---
+++ 2017/Fall2017/SEM/SEM4/Задание про счёт цифр.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int count1 = 1;
+            int numEnd =0;
+            int num;
+            Console.Write("Введите число n - длину массива: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите первый член массива: ");
+            int num1 = int.Parse(Console.ReadLine());
+            
+            for (int i=2; i <= n; i++)
+            {
+                Console.Write("Введите " + i + "-ый член массива нажимая enter: ");
+                num = int.Parse(Console.ReadLine());
+                if (num == num1) count1++;
+                else count1 = 1;
+                num1 = num;
+                if (count1 > count)
+                {
+                    count = count1;
+                    numEnd = num;
+                }
+            }
+            Console.Write("Подмассив:");
+            for (int j = 0; j < count; j++)
+                Console.Write(numEnd);
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ 2017/Fall2017/SEM/SEM4/Задание про число.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число 1<=n<=27: ");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for(int a = 1; a<10; a++)
+            {
+                for(int b = 0; b < 10; b++)
+                {
+                    for(int c=0; c<10; c++)
+                    {
+                        if ((a + b + c) == n) count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
