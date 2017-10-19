8a86a3d32219b70586966c8e6c51eb112da3e57d
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-10-20 00:09:38 +0300

    Add files via upload

---
+++ 2017/Fall2017/SEM/SEM5/Задание 2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number, which have less than 100 signs: ");
+            long k = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter degree, в которой записано число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter degree, в которйо хотите получить число: ");
+            int b = int.Parse(Console.ReadLine());
+            long[] secondArray = new long[100];
+            long[] firstArray = new long[100];
+            long[] thurdArray = new long[100];
+            long mainNum = 0;
+            int r = 0;
+            while (k > 0)
+            {
+                firstArray[r] = k % 10;// переводим число из степени а, в 10-ую
+                mainNum += (firstArray[r] * (int)Math.Pow(a, r));
+                k /= 10;
+                r++;
+            }
+            int countNum = 0;
+            int i = 0;
+            while (mainNum > 0)
+            {
+                secondArray[i] = (mainNum % b);//находим b-ую запись 10-ого числа
+                mainNum /= b;
+                i++;
+                countNum++;
+            }
+            Console.Write("Number in degree b is:");
+            for (int y = 0; y < countNum; y++)
+            {
+                thurdArray[y] = secondArray[countNum - y - 1];
+                Console.Write(thurdArray[y]);
+            }
+
+        }
+    }
+}
