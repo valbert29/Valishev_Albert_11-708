d32356de5c61f943dcb1e98609fdf87394b04a93
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-11-01 22:33:59 +0400

    SomeChanges

---
+++ 2017/Fall2017/PS/PS1/Semestrovka1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)//в этой задаче надо определить, можно ли пойти конем на заданную точку
+        {
+            Console.WriteLine("введите x фигуры: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите y фигуры: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите x проверяемой точки: ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите y проверяемой точки: ");
+            int y1 = int.Parse(Console.ReadLine());
+            if ((Math.Abs(x1 - x) == 2 && Math.Abs(y1 - y) == 1) || (Math.Abs(x1 - x) == 1 && Math.Abs(y1 - y) == 2))
+            {
+                Console.WriteLine("YES");
+            }
+            else Console.WriteLine("NO");
+
+        }
+    }
+}
