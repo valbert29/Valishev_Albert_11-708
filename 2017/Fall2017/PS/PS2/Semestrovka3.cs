00e38c66b6ec7951c0c46d6d2898cb23dfb57765
valbert29 <31650207+valbert29@users.noreply.github.com>
2017-11-15 20:46:44 +0400

    semestr

---
+++ 2017/Fall2017/PS/PS2/Semestrovka3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The1Point3Task
+{
+    class Program
+    {
+        static void Main(string[] args)//exercise 1.3
+        {
+            Console.WriteLine("Введите x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите точность: ");
+            double e = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сумма последовательности - {0}",CalculateGraduatedNum(e, x));
+        }
+        public static double CalculateGraduatedNum(double e, int x)
+        {
+            double sequence = 1;
+            double k = 1;
+            int step = 0;
+            while (k > e)
+            {
+                step++;
+                k *= (x * Math.Log(3) / step);
+                sequence += k;
+            }
+            Console.WriteLine("Шагов - {0}", step);
+            return sequence;
+        }
+    }
+}
---
+++ 2017/Fall2017/PS/PS2/Semestrovka4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите точность: ");//Exercise 2
+            double e = double.Parse(Console.ReadLine());
+            Console.WriteLine(FindCos(e, 0.0));
+            
+        }
+        public static double FindCos(double e, double x)
+        {
+            double value = Math.Cos(x);
+            if (Math.Abs(value - x) < e) return value;
+            return FindCos(e, value);
+        }
+    }
+}
