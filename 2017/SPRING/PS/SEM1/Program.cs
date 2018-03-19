27b44aa730922ea6084cd648999a3996cd0e4fa7
valbert29 <31650207+valbert29@users.noreply.github.com>
2018-03-19 14:05:53 +0400

    Update Program.cs

--- 2017/SPRING/PS/SEM1/Program.cs
+++ 2017/SPRING/PS/SEM1/Program.cs
@@ -14,29 +14,38 @@ namespace Semestr1
         {
 
             var myList = new FigureList();
-            var helper = new Tools();
+            var helper = new Tool();
             helper.MyList = myList;
 
-            myList.Add(new Figure(1, 2, 3, 4, 5, 6), 0);
-            myList.Add(new Figure(2, 2, 3, 3, 5, 6), 1);
-            myList.Add(new Figure(3, 2, 3, 4, 5, 6), 2);
+            //вставки некоторой фигуры в некоторую позицию списка 
+            //(при вставке учесть существует ли подобный элемент в списке, 
+            //если да – то заменить существующий, например, может поменяться цвет фигуры);
+            myList.AddByIndex(new Figure(1, 2, 3, 4, 5, 6), 0);
+            myList.AddByIndex(new Figure(2, 2, 3, 3, 5, 6), 1);
+            myList.AddByIndex(new Figure(3, 2, 3, 4, 5, 6), 2);
             myList.Remove(new Figure(1, 2, 3, 4, 5, 6));
             Console.WriteLine("basic methods:");
             helper.Decode();
-                        
-            myList = helper.RectangleCoordinates(new Figure(1, 2, 3, 4, 5, 6));
+
+            //Построить новый список, состоящий из фигур, которые имеют общие точки с некоторым прямоугольником
+            myList = helper.CreateByRectangleCoordinates(new Figure(1, 2, 3, 4, 5, 6));
             Console.WriteLine("rectCoordsList:");
             helper.Decode();
-            
-            myList = helper.Encode();
+
+            //построение списка по множеству фигур, заданному набором строк в некотором текстовом файле;
+            myList = helper.Encode(@"C:\Users\Пользователь\Desktop\TEXT.txt");
             Console.WriteLine("Encode:");
             helper.Decode();
 
-            helper.Encode();
+            //Каждый отрезок с координатами(X1, Y1); (X2, Y2) при условии, что X1<>X2 и Y1<> Y2, 
+            //заменить прямоугольником  некоторого цвета, в противном случае удалить из списка  
+            helper.Encode(@"C:\Users\Пользователь\Desktop\TEXT.txt");
             myList = helper.LinesToRectangle();
             Console.WriteLine("lineToRectList:");
             helper.Decode();
 
+            //Построить новый список из фигур,
+            //площади которых более некоторой константы(задается пользователем в интерактивном режиме).
             Console.WriteLine("Введите констaнту:");
             int n = int.Parse(Console.ReadLine());
             helper.BuildListByConst(n);
